062ba30b2e570922b55736e37c388a1fc2aeabec
bolomeboy <bolobeats@outlook.com>
2026-03-11 18:48:48 -0700

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -21,7 +21,7 @@ https://github.com/nmelhado/league-page/blob/master/TRAINING_WHEELS.md#ii-adding
 
 export const managers = [
 {
-  "roster": 732848788863037440,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
+  "roster": 1,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
   "name": "Gavin",
   "tookOver": null, // (optional) used if a manager took over a team, delete this line or change to null otherwise
   "location": "Manteca, CA", // (optional)
