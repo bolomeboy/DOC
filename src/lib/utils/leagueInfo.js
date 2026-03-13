d2e1d70d6118116706bdc5d6fd7431b7dc420e03
bolomeboy <bolobeats@outlook.com>
2026-03-12 17:14:28 -0700

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -143,11 +143,11 @@ export const managers = [
   "roster": 6,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
   "name": "Frankie",
   "tookOver": null, // (optional) used if a manager took over a team, delete this line or change to null otherwise
-  "location": "Brooklyn", // (optional)
+  "location": "Manteca, CA", // (optional)
   "bio": "Lorem ipsum...",
   "photo": "/managers/name.jpg", // square ratio recommended (no larger than 500x500)
   "fantasyStart": 2025, // (optional) when did the manager start playing fantasy football
-  "favoriteTeam": "nyj", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
+  "favoriteTeam": "lv", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
   "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
   "rival": {
     name: "Rival", // Can be anything (usually your rival's name)
@@ -187,11 +187,11 @@ export const managers = [
   "roster": 8,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
   "name": "Tiago",
   "tookOver": null, // (optional) used if a manager took over a team, delete this line or change to null otherwise
-  "location": "Brooklyn", // (optional)
+  "location": "Manteca, CA", // (optional)
   "bio": "Lorem ipsum...",
   "photo": "/managers/name.jpg", // square ratio recommended (no larger than 500x500)
   "fantasyStart": 2025, // (optional) when did the manager start playing fantasy football
-  "favoriteTeam": "sf", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
+  "favoriteTeam": "null", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
   "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
   "rival": {
     name: "Rival", // Can be anything (usually your rival's name)
@@ -231,7 +231,7 @@ export const managers = [
   "roster": 10,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
   "name": "Paul",
   "tookOver": null, // (optional) used if a manager took over a team, delete this line or change to null otherwise
-  "location": "CA", // (optional)
+  "location": "Turlock, CA", // (optional)
   "bio": "Lorem ipsum...",
   "photo": "/managers/name.jpg", // square ratio recommended (no larger than 500x500)
   "fantasyStart": 2025, // (optional) when did the manager start playing fantasy football
@@ -253,7 +253,7 @@ export const managers = [
   "roster": 11,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
   "name": "Rui",
   "tookOver": null, // (optional) used if a manager took over a team, delete this line or change to null otherwise
-  "location": "CA", // (optional)
+  "location": "San Jose, CA", // (optional)
   "bio": "Lorem ipsum...",
   "photo": "/managers/name.jpg", // square ratio recommended (no larger than 500x500)
   "fantasyStart": 2025, // (optional) when did the manager start playing fantasy football
@@ -275,7 +275,7 @@ export const managers = [
   "roster": 12,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
   "name": "Mitch",
   "tookOver": null, // (optional) used if a manager took over a team, delete this line or change to null otherwise
-  "location": "CA", // (optional)
+  "location": "San Diego, CA", // (optional)
   "bio": "Lorem ipsum...",
   "photo": "/managers/name.jpg", // square ratio recommended (no larger than 500x500)
   "fantasyStart": 2025, // (optional) when did the manager start playing fantasy football
