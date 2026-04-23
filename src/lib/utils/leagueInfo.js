d517ab964569dbdd5e2292225330fff882381a99
bolomeboy <bolobeats@outlook.com>
2026-04-22 18:06:31 -0700

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -190,7 +190,7 @@ export const managers = [
   "tookOver": null, // (optional) used if a manager took over a team, delete this line or change to null otherwise
   "location": "Manteca, CA", // (optional)
   "bio": "Lorem ipsum...",
-  "photo": "/managers/tiago.jpg", // square ratio recommended (no larger than 500x500)
+  "photo": "/managers/tiago.png", // square ratio recommended (no larger than 500x500)
   "fantasyStart": 2025, // (optional) when did the manager start playing fantasy football
   "favoriteTeam": "null", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
   "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
@@ -212,7 +212,7 @@ export const managers = [
   "tookOver": null, // (optional) used if a manager took over a team, delete this line or change to null otherwise
   "location": "San Jose, CA", // (optional)
   "bio": "Lorem ipsum...",
-  "photo": "/managers/tony.jpg", // square ratio recommended (no larger than 500x500)
+  "photo": "/managers/tony.png", // square ratio recommended (no larger than 500x500)
   "fantasyStart": 2025, // (optional) when did the manager start playing fantasy football
   "favoriteTeam": "sf", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
   "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
